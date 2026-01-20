6c9499f5ad4f3a9ebee29650e1ec5d17bee4c421
Winner <blessedwinner66@gmail.com>
2026-01-20 16:17:02 +0200

    Added lombok annotations

--- src/main/java/com/xenon/Resource_manager/Resource.java
+++ src/main/java/com/xenon/Resource_manager/Resource.java
@@ -1,10 +1,20 @@
 package com.xenon.Resource_manager;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "resources")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Resource {
     @Id
     @GeneratedValue
--- src/main/java/com/xenon/Resource_manager/User.java
+++ src/main/java/com/xenon/Resource_manager/User.java
@@ -1,6 +1,10 @@
 package com.xenon.Resource_manager;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,6 +13,10 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue
